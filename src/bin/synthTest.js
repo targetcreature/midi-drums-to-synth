b3cb69c055c52b648fe0e85ca4770410c0e9bec4
Craig Hildebrand <targetcreature@gmail.com>
2019-04-24 12:49:50 -0700

    dictionary hz, renamed synth

--- src/bin/synthTest.js
+++ src/bin/synthTest.js
@@ -2,39 +2,38 @@ var NodeSynth = require("nodesynth")
 
 require("nodesynth/notes")
 
-var ns = new NodeSynth.Synth()
-ns.play()
+var synth = new NodeSynth.Synth()
+synth.play()
 
 const waves = [
     "sine",
+    "triangle",
     "cosine",
     "square",
-    "triangle",
     "sawtooth"
 ]
 
-const hz = [
-    ["C4", 261.6],
-    ["D4", 293.7],
-    ["E4", 329.6],
-    ["F4", 349.2],
-    ["A4", 440],
-    ["B4", 493.9]
-]
+const hz = {
+    C4: 261.6,
+    D4: 293.7,
+    E4: 329.6,
+    F4: 349.2,
+    A4: 440,    
+    B4: 493.9
+}
 
-const freq = hz[0][1]
-const wave = waves[2]
+const freq = hz.D4
+const wave = waves[0]
 
 const presets = {
     // name: [slope, rate]
     bounce: [500, 50]
 }
 
-const mod = presets.bounce
+const [slope, rate] = presets.bounce
 
-var play = new NodeSynth.Oscillator(wave, (t) => {
-    const [slope, rate] = mod    
+var note = new NodeSynth.Oscillator(wave, (t) => {
     return freq + t*slope % rate
 })
 
-ns.source = play
+synth.source = note
